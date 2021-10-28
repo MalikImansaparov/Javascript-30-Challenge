d3c193259898348b6b12f3fce438188e5f56d16f
imansaparov <malikimansaparov@gmail.com>
2021-10-28 17:14:27 +0600

    feat: add JS and CSS clock task

---
+++ 02 - JS and CSS Clock/script.js
@@ -0,0 +1,21 @@
+ const secondHand = document.querySelector('.second-hand');
+  const minsHand = document.querySelector('.min-hand');
+  const hourHand = document.querySelector('.hour-hand');
+
+  function setDate(){
+  const now = new Date()
+ 
+  const second = now.getSeconds()
+  const secDegree = ((second / 60) * 360) + 90
+  secondHand.style.transform = `rotate(${secDegree}deg)`
+
+  const minute = now.getMinutes()
+  const minDegree = ((minute / 60) * 360) + 90
+  minsHand.style.transform = `rotate(${minDegree}deg)`
+
+  const hour = now.getMinutes()
+  const hourDegree = ((minute / 12) * 360) + 90
+  hourHand.style.transform = `rotate(${hourDegree}deg)`
+}
+
+setInterval(setDate,1000)
