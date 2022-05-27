c4d7712afc3eb8186276d290deab77f6180fb08f
imansaparov <malikimansaparov@gmail.com>
2022-05-27 21:57:56 +0600

    feat: update JS and CSS Clock task

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,6 +1,6 @@
  const secondHand = document.querySelector('.second-hand');
-  const minsHand = document.querySelector('.min-hand');
-  const hourHand = document.querySelector('.hour-hand');
+ const minsHand = document.querySelector('.min-hand');
+ const hourHand = document.querySelector('.hour-hand');
 
   function setDate(){
   const now = new Date()
